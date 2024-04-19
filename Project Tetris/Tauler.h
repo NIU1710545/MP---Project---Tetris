18c611e29f391a58e67b2f25d0638383009e9a81
NIU1710545 <laialishuang.orus@autonoma.cat>
2024-04-19 20:57:03 +0200

    Declaració de la classe Tauler #Primera configuració d'atributs i mètodes per poder implementar la classe en el joc

--- Project Tetris/Tauler.h
+++ Project Tetris/Tauler.h
@@ -7,5 +7,35 @@ const int MAX_COL = 8;
 
 class Tauler
 {
+public:
+	Tauler();
+	~Tauler();
+
+	bool colocarFigura(Figura figura, int fila, int columna);
+	bool moureFigura(Figura figura, int columna);
+	bool girarFigura(Figura figura);
+	void eliminarFilesCompletades();
+	bool taulerPle();
+
+private:
+	int m_tauler[MAX_FILA][MAX_COL];
+
+	void inicialitzarTauler();
+
 };
+
+/*
+ - Ha de servir per poder gestionar el tauler de joc. 
+ - Ha de poder guardar quines caselles del tauler estan ocupades per figures ja col·locades i per la 
+   figura actual que està caient. 
+ - Ha de tenir tots els mètodes necessaris per poder comprovar que els moviments i girs de la 
+   figura són vàlids, col·locar la figura a la seva posició final i eliminar les files que es van 
+   completant. 
+ - Penseu bé quins mètodes públics ha de tenir aquesta classe. Poseu només com a mètodes 
+   públics els que sigui necessari utilitzar des de la classe Joc. 
+ - Si és necessari, descomposeu els mètodes públics en altres mètodes més simples. Definiu com 
+   a privats aquests mètodes auxiliars.
+*/
+
+
 #endif
