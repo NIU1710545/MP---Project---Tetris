69f76ceb70a629d98c72c0505142876fb432fb5c
NIU1710545 <laialishuang.orus@autonoma.cat>
2024-04-20 13:46:58 +0200

    Inicialització dels mètodes de JOC #Només centrat en el mètode inicialitza

--- Project Tetris/Joc.cpp
+++ Project Tetris/Joc.cpp
@@ -1,2 +1,32 @@
 #include "Joc.h"
 
+Joc::Joc() 
+{
+
+}
+
+void inicialitza(const string& nomFitxer)
+{
+	m_figura.inicialitzaio(nomFitxer);
+	m_tauler.inicialitzacio(nomFitxer);
+}
+
+bool giraFigura(DireccioGir direccio)
+{
+
+}
+
+bool mouFigura(int dirX)
+{
+
+}
+
+int baixaFigura()
+{
+
+}
+
+void escriuTauler(const string& nomFitxer)
+{
+
+}
